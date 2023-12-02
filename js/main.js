fb275c6e6a86bd1a85ff9836e8f3d68a6bae9c7d
jsdeveloper0401 <wolfmaks85@gmail.com>
2023-12-02 22:48:53 +0500

    changed html and style css

--- js/main.js
+++ js/main.js
@@ -128,3 +128,26 @@ for (let i = 0; i < tabs.length; i++) {
         getActiveTab(i);
     });
 }
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
