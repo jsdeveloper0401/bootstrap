4581809722f0a7e5eba958f1340a2ab465a05a5a
jsdeveloper0401 <wolfmaks85@gmail.com>
2023-12-03 19:50:46 +0500

    changed style and html

--- js/main.js
+++ js/main.js
@@ -1,104 +1,96 @@
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+    if (
+        document.body.scrollTop > 200 ||
+        document.documentElement.scrollTop > 200
+    ) {
+        backtop.classList.add("backtop-show");
+    } else {
+        backtop.classList.remove("backtop-show");
+    }
+}
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 1000;
+
+setTimeout(() => {
+    loading.classList.add("loading-none");
+}, loadingDuration);
+
+window.addEventListener("scroll", function () {
+    toggleBacktop();
+});
+
 // light-dark start
 let modeBtn = document.getElementById("mode__btn");
 
 modeBtn.addEventListener("click", function () {
-  if (document.body.className != "dark") {
-    this.firstElementChild.src = "images/light.svg";
-  } else {
-    this.firstElementChild.src = "images/dark.svg";
-  }
-  document.body.classList.toggle("dark");
+    if (document.body.className != "dark") {
+        this.firstElementChild.src = "../images/light.svg";
+    } else {
+        this.firstElementChild.src = "../images/dark.svg";
+    }
+    document.body.classList.toggle("dark");
 });
 // light dark end
 
 // owl start
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-      margin: 0,
-      center: true,
-      loop: true,
-      items: 1,
-      nav: true,
-      navText: ['<img src="images/prev.svg"/>', '<img src="images/next.svg"/>'],
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      responsive: {
-          0: {
-              items: 1,
-              dots: false,
-          },
-          800: {
-              items: 2,
-          },
-          1150: {
-              items: 3,
-          },
-      },
-  });
+    $(".owl-carousel").owlCarousel({
+        margin: 0,
+        center: true,
+        loop: true,
+        items: 1,
+        nav: true,
+        navText: [
+            '<img src="images/prev.svg"/>',
+            '<img src="images/next.svg"/>',
+        ],
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: {
+            0: {
+                items: 1,
+                dots: false,
+            },
+            800: {
+                items: 2,
+            },
+            1150: {
+                items: 3,
+            },
+        },
+    });
 });
 // owl end
 
-// navbar shrink
-// navbar shrink
-window.addEventListener("scroll", function () {
-  shrink();
-});
-
-let navbar = document.getElementById("navbar");
-
-function shrink() {
-  if (scrollY > 100) {
-    navbar.classList.add("navbar-shrink");
-  } else {
-    navbar.classList.remove("navbar-shrink");
-  }
-}
-// navbar shrink end
 // navbar shrink end
 
 // toggle start
 function openNavbar() {
-  document.getElementById("navbar-responsive").style.left = "0";
-  // document.getElementById("navbar-responsive").style.top = "0";
+    document.getElementById("navbar-responsive").style.left = "0";
+    // document.getElementById("navbar-responsive").style.top = "0";
 }
 function closeNavbar() {
-  document.getElementById("navbar-responsive").style.left = "-100%";
-  // document.getElementById("navbar-responsive").style.top = "-100%";
+    document.getElementById("navbar-responsive").style.left = "-100%";
+    // document.getElementById("navbar-responsive").style.top = "-100%";
 }
 
 document.getElementById("navbar-open").addEventListener("click", openNavbar);
 document.getElementById("navbar-close").addEventListener("click", closeNavbar);
 // toggle end
 // backtop start
-window.addEventListener("scroll", function () {
-  toggleBacktop();
-});
 
-let backtop = document.getElementById("backtop");
-
-function toggleBacktop() {
-  if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
-  ) {
-    backtop.classList.add("backtop-show");
-  } else {
-    backtop.classList.remove("backtop-show");
-  }
-}
 // backtop end
 
 // aos start
-  AOS.init();
+AOS.init();
 // aos end
 //loading start//
-const loading = document.getElementById("loading");
-
-const loadingDuration = 500; // 1s
-setTimeout(() => {
-    loading.classList.add("loading-none");
-}, loadingDuration);
+// loading start
 
 const tabs = document.querySelectorAll(".ap-tab-link");
 const tabs_content = document.querySelectorAll(".ap-tab-content");
@@ -130,24 +122,39 @@ for (let i = 0; i < tabs.length; i++) {
 }
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-  "use strict";
+    "use strict";
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll(".needs-validation");
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+        form.addEventListener(
+            "submit",
+            (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
 
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
+                form.classList.add("was-validated");
+            },
+            false
+        );
+    });
 })();
+
+//navbar-shrink//
+window.addEventListener("scroll", function () {
+    shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+    if (scrollY > 100) {
+        navbar.classList.add("navbar-shrink");
+    } else {
+        navbar.classList.remove("navbar-shrink");
+    }
+}
